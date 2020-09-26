22cb0539720c893fc856d0f26b5ab6af7fd06b58
Hugo <UO265357@uniovi.es>
2020-09-26 11:31:50 +0200

    cambio 3 luna

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -5,7 +5,7 @@ public class Demo {
 	
 	public static void main(String[] args) {
 		System.out.println("linea1");
-		System.out.println("linea2");
+		System.out.println("cambio 3 luna");
 		System.out.println("linea3");
 		/* cambio desde mars */
 	}
